074260b4788ec8da4e604e9657a0afdc81ee7916
TylerBiggert <TylerB.Other@gmail.com>
2019-08-18 07:53:16 -0400

    Removed unused variable

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { players } from './Players'; //destructured because I will add more variables in future
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
